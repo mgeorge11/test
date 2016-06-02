2340004096410d7113149ab62c91573406cca307
mgeorge11 <mike.thomas34@ymail.com>
2016-06-02 17:57:09 -0400

    Add files via upload

---
+++ testForProjects.java
@@ -0,0 +1,12 @@
+package test1234;
+
+public class testForProjects {
+	public static void tester(){
+		System.out.print("Hello World!");
+	}
+	
+	public static void main(String[] args) {
+		tester();
+	}
+
+}
