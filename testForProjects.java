53413f796b8874a0d6752435df6ba2af93cf7b8a
mgeorge11 <mike.thomas34@ymail.com>
2016-06-02 18:51:11 -0400

    Update testForProjects.java

--- testForProjects.java
+++ testForProjects.java
@@ -5,6 +5,14 @@ public class testForProjects {
 		System.out.print("Hello World!");
 		System.out.print("Kyle can you edit this document?")
                 Boolean canSee = true;
+                if(canSee == true){
+                 System.out.println("Then we ar good to go!");
+                }
+                else{
+                	System.out.print("Well shit");
+                }
+                
+                
 	}
 	
 	public static void main(String[] args) {
