5ab1dc652d2b3f9eaf672516d113f119226aaec3
redscissors3 <kyleschoep@gmail.com>
2016-06-02 18:39:33 -0400

    Update testForProjects.java

--- testForProjects.java
+++ testForProjects.java
@@ -4,6 +4,7 @@ public class testForProjects {
 	public static void tester(){
 		System.out.print("Hello World!");
 		System.out.print("Kyle can you edit this document?")
+                Boolean canSee = true;
 	}
 	
 	public static void main(String[] args) {
