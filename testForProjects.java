9ba9a2aea9d5249cdb217a3e83ea4104fca70af8
mgeorge11 <mike.thomas34@ymail.com>
2016-06-02 18:15:40 -0400

    Update testForProjects.java

--- testForProjects.java
+++ testForProjects.java
@@ -3,6 +3,7 @@ package test1234;
 public class testForProjects {
 	public static void tester(){
 		System.out.print("Hello World!");
+		System.out.print("Kyle can you edit this document?")
 	}
 	
 	public static void main(String[] args) {
